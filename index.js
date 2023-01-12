e4f1e51a73a622114bdd1d947c6d45c5ca9873e1
Brent Reininger <brent.reininger89@gmail.com>
2023-01-11 18:07:57 -0800

    Add test function to test coefficient of determination

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const tf = require("@tensorflow/tfjs");
 const loadCSV = require("./load-csv");
 const LinearRegression = require("./linear-regression");
 
-let { features, labels, testFeatures, testLabes } = loadCSV("./cars.csv", {
+let { features, labels, testFeatures, testLabels } = loadCSV("./cars.csv", {
   shuffle: true,
   splitTest: 50,
   dataColumns: ["horsepower"],
@@ -16,10 +16,6 @@ const regression = new LinearRegression(features, labels, {
 });
 
 regression.train();
+const r2 = regression.test(testFeatures, testLabels);
 
-console.log(
-  "Updated M is:",
-  regression.weights.get(1, 0),
-  "Updated B is:",
-  regression.weights.get(0, 0)
-);
+console.log("R2 is", r2);
