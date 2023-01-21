4ebd551414115657ef57efc72bb5f64586e8505f
Brent Reininger <brent.reininger89@gmail.com>
2023-01-20 16:49:46 -0800

    Enable logistic regression

--- logistic-regression/index.js
+++ logistic-regression/index.js
@@ -2,6 +2,7 @@ require("@tensorflow/tfjs-node");
 const tf = require("@tensorflow/tfjs");
 const { values } = require("lodash");
 const loadCSV = require("../load-csv");
+const LogisticRegression = require("./logistic-regression");
 
 const { features, labels, testFeatures, testLabels } = loadCSV(
   "../data/cars.csv",
@@ -18,4 +19,16 @@ const { features, labels, testFeatures, testLabels } = loadCSV(
   }
 );
 
-console.log(labels);
+const regression = new LogisticRegression(features, labels, {
+  learningRate: 0.5,
+  iterations: 100,
+  batchSize: 50,
+});
+
+regression.train();
+regression
+  .predict([
+    [130, 307, 1.75],
+    [88, 97, 1.065],
+  ])
+  .print();
