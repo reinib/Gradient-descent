d1d318abeae08262ea358142d25732b6c8e53606
Brent Reininger <brent.reininger89@gmail.com>
2023-01-25 16:19:05 -0800

    Refactor for image recognition

--- multinominal-logistic-regression/index.js
+++ multinominal-logistic-regression/index.js
@@ -1,39 +1,17 @@
 require("@tensorflow/tfjs-node");
 const tf = require("@tensorflow/tfjs");
-const loadCSV = require("../load-csv");
 const LogisticRegression = require("./logistic-regression");
 const plot = require("node-remote-plot");
 const _ = require("lodash");
+const mnist = require("mnist-data");
 
-const { features, labels, testFeatures, testLabels } = loadCSV(
-  "../data/cars.csv",
-  {
-    dataColumns: ["horsepower", "displacement", "weight"],
-    labelColumns: ["mpg"],
-    shuffle: true,
-    splitTest: 50,
-    converters: {
-      mpg: (value) => {
-        const mpg = parseFloat(value);
-        if (mpg < 15) {
-          return [1, 0, 0];
-        } else if (mpg < 30) {
-          return [0, 1, 0];
-        } else {
-          return [0, 0, 1];
-        }
-      },
-    },
-  }
-);
+const mnistData = mnist.training(0, 10);
 
-const regression = new LogisticRegression(features, _.flatMap(labels), {
-  learningRate: 0.5,
-  iterations: 100,
-  batchSize: 10,
-  decisionBoundary: 0.5,
+const features = mnistData.images.values.map((image) => _.flatMap(image));
+const encodedLabels = mnistData.labels.values.map((label) => {
+  const row = new Array(10).fill(0);
+  row[label] = 1;
+  return row;
 });
 
-regression.train();
-
-console.log(regression.test(testFeatures, _.flatMap(testLabels)));
+console.log(encodedLabels);
